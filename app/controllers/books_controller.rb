e5af25aace46177e8df93f62e5ce2e002f063e1f
uavpliyc <uavpliyc@gmail.com>
2021-03-02 08:52:45 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,8 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
+    @book_comments = @book.book_comments
+    @book_comment = BookComment.new
   end
 
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_024849) do
+ActiveRecord::Schema.define(version: 2021_03_02_034308) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_book_comments_on_book_id"
+    t.index ["user_id"], name: "index_book_comments_on_user_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +28,16 @@ ActiveRecord::Schema.define(version: 2021_03_02_024849) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_comments_on_book_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "favorites", force: :cascade do |t|
